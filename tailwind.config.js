d1b14259f59f38d71401f618226479c9bd91d480
FahadKabir-repliq <fahad.kabir@repliq.dev>
2023-06-25 11:43:24 +0600

    neocon group project added in github

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    colors: {
+      ksl_light_red_200: "#FFF4F4",
+    },
+  },
+  plugins: [],
+};
