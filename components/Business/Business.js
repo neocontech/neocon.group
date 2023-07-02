184c125ac258fe93abbe5c9360d8a2c2593c1dbd
FahadKabir-repliq <fahad.kabir@repliq.dev>
2023-07-02 16:31:16 +0600

    Business page added

---
+++ components/Business/Business.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Image from "next/image";
+import Contact from "../HomePage/Contact";
+import Concern1 from "../../public/assets/concerns/Rectangle1.png";
+import Concern2 from "../../public/assets/concerns/Rectangle2.png";
+import Concern3 from "../../public/assets/concerns/Rectangle3.png";
+const CorncernsAll = [
+  { id: 1, image: Concern1 },
+  { id: 2, image: Concern2 },
+  { id: 3, image: Concern3 },
+  { id: 4, image: Concern1 },
+];
+
+function Business() {
+  return (
+    <>
+      <div>
+        <div className="pt-20">
+          <div className="grid grid-cols-2 xsm:grid-cols-1 sm:grid-cols-1  gap-6 py-5">
+            {CorncernsAll.map((company, index) => (
+              <div key={index}>
+                <div className="broder rounded-xl border-ngl_orange">
+                  <Image
+                    src={company.image}
+                    alt="company png"
+                    className="w-auto h-auto"
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <Contact />
+      </div>
+    </>
+  );
+}
+
+export default Business;
---
+++ components/Business/BusinessMainsection.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "./Hero";
+import Business from "./Business";
+import Tabs from "./Tabs";
+
+function BusinessMainsection() {
+  return (
+    <div>
+      <Hero />
+      <Tabs />
+    </div>
+  );
+}
+
+export default BusinessMainsection;
