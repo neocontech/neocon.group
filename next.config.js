634eaff70a9b1027927287b6800a1b53f09d17fd
FahadKabir-repliq <fahad.kabir@repliq.dev>
2023-07-05 17:01:10 +0600

    env backend url change

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: false,
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "**.neocongroup.com",
-//       },
-//     ],
-//   },
-// };
-
-// module.exports = nextConfig;
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.neocongroup.com",
+      },
+    ],
   },
 };
 
 module.exports = nextConfig;
+// const nextConfig = {
+//   reactStrictMode: false,
+//   images: {
+//     domains: ["localhost"],
+//   },
+// };
+
+// module.exports = nextConfig;
